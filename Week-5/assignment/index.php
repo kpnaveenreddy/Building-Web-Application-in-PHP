03fa39e19e34f2172ac1369b1a0be8903f08a253
K P Naveen Reddy <kpnaveenreddy1999@gmail.com>
2020-06-05 08:52:14 -0700

    Add files via upload

---
+++ Week-5/assignment/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>K P Naveen Reddy PHP</title>
+	</head>
+	<body>
+		<h1>K P Naveen Reddy PHP</h1>
+
+		<pre>ASCII ART (First letter of the Name 'Naveen' - 'N'):
+
+     ******       **
+     **   **      **
+     **    **     **
+     **     **    **
+     **      **   **
+     **       **  **
+     **        *****
+		</pre>
+
+		<p>
+			The SHA256 hash of "K P Naveen REddy" is <?php print hash('sha256', 'K P Naveen Reddy'); ?>
+		</p>
+<a href="check.php">Click here to check the errors settings</a>
+<br>
+<a href="fail.php">Click here to cause a traceback</a>
+
+	</body>
+</html>
